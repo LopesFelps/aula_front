54fa623aa67e1f7270989d345af885cbbd7b2e81
Felipe Lopes <lopes.felipe24@outlook.com>
2025-08-28 14:46:56 -0300

    feat: adiciona card

--- felipe/src/App.js
+++ felipe/src/App.js
@@ -1,34 +1,33 @@
-import React from "react";
-import Button from "./Components/Button/Button";
+// import React from "react";
+// import Button from "./Components/Button/Button";
 
-function App() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h2>Exemplo de Botões</h2>
+// function App() {
+//   return (
+//     <div style={{ padding: "20px" }}>
+//       <h2>Exemplo de Botões</h2>
 
-      <Button text="Salvar" />
+//       <Button text="Salvar" />
 
-      <Button text="Excluir" bgColor="green" textColor="white" />
+//       <Button text="Excluir" bgColor="green" textColor="white" />
 
-      <Button
-        text="Cancelar"
-        bgColor="red"
-        textColor="yellow"
-        onClick={() => alert("Você clicou no botão vermelho!")}
-      />
+//       <Button
+//         text="Cancelar"
+//         bgColor="red"
+//         textColor="yellow"
+//         onClick={() => alert("Você clicou no botão vermelho!")}
+//       />
 
-      <Button
-        text="Novo"
-        bgColor="purple"
-        textColor="white"
-        onClick={() => console.log("Botão roxo foi clicado!")}
-      />
-    </div>
-  );
-}
+//       <Button
+//         text="Novo"
+//         bgColor="purple"
+//         textColor="white"
+//         onClick={() => console.log("Botão roxo foi clicado!")}
+//       />
+//     </div>
+//   );
+// }
 
-export default App;
-
+// export default App;
 
 // import Person from './Components/Person/Person';
 // function App() {
@@ -41,34 +40,72 @@ export default App;
 // }
 // export default App;
 
+import React from "react";
+import ProfileCard from "./Components/ProfileCard/ProfileCard";
 
-// import React from "react";
-// import ProfileCard from "./ProfileCard";
+function App() {
+  const profiles = [
+    {
+      name: "Jorginho",
+      role: "Professor",
+      image: "https://randomuser.me/api/portraits/men/1.jpg",
+    },
+    {
+      name: "Evandrinho",
+      role: "Engenheiro",
+      image: "https://randomuser.me/api/portraits/men/2.jpg",
+    },
+    {
+      name: "Lucrecio",
+      role: "Designer",
+      image: "https://avatars.dicebear.com/api/adventurer/lucrecio.svg",
+    },
+    {
+      name: "Nicanor",
+      role: "Ator",
+      image: "https://avatars.dicebear.com/api/avataaars/nicanor.svg",
+    },
+    {
+      name: "Givanildo",
+      role: "Bombeiro",
+      image: "https://avatars.dicebear.com/api/adventurer/floresbela.svg",
+    },
+    {
+      name: "Jujuzete",
+      role: "Cuteleiro",
+      image: "https://randomuser.me/api/portraits/women/3.jpg",
+    },
+    {
+      name: "Juca",
+      role: "Ourives",
+      image: "https://randomuser.me/api/portraits/men/4.jpg",
+    },
+    {
+      name: "Predino",
+      role: "Pedreiro",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      name: "Josefino",
+      role: "Pintor",
+      image: "https://randomuser.me/api/portraits/men/7.jpg",
+    },
+  ];
 
-// function App() {
-//   const profiles = [
-//     { name: "Jurandir", role: "Professor", image: "https://randomuser.me/api/portraits/men/1.jpg" },
-//     { name: "Dejair", role: "Engenheiro", image: "https://randomuser.me/api/portraits/men/2.jpg" },
-//     { name: "Lucrecio", role: "Designer", image: "https://avatars.dicebear.com/api/adventurer/lucrecio.svg" },
-//     { name: "Nicanor", role: "Ator", image: "https://avatars.dicebear.com/api/avataaars/nicanor.svg" },
-//     { name: "Florisbela", role: "Bombeiro", image: "https://avatars.dicebear.com/api/adventurer/floresbela.svg" },
-//     { name: "Jujuzete", role: "Cuteleiro", image: "https://randomuser.me/api/portraits/women/3.jpg" },
-//     { name: "Juca", role: "Ourives", image: "https://randomuser.me/api/portraits/men/4.jpg" },
-//     { name: "Predino", role: "Pedreiro", image: "https://randomuser.me/api/portraits/men/5.jpg" },
-//   ];
+  return (
+    <div
+      style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+    >
+      {profiles.map((profile, index) => (
+        <ProfileCard
+          key={index}
+          name={profile.name}
+          role={profile.role}
+          image={profile.image}
+        />
+      ))}
+    </div>
+  );
+}
 
-//   return (
-//     <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-//       {profiles.map((profile, index) => (
-//         <ProfileCard
-//           key={index}
-//           name={profile.name}
-//           role={profile.role}
-//           image={profile.image}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
